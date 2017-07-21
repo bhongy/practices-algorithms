f00670509bd8953a50f1db1ff2776d964303b95c
Thanik Bhongbhibhat <bhongbhibhat@gmail.com>
2017-07-21 08:04:08 -0700

    Add exercise: Reverse String

---
+++ src/reverse-string.test.js
@@ -0,0 +1,17 @@
+// @flow
+
+import reverseString from './reverse-string';
+
+describe('Reverse String', () => {
+  // it('should return an empty string if input is an empty string')
+  // it('should return an empty string if input not a type string')
+  it('should return a reversed string correctly', () => {
+    [
+      ['', ''],
+      ['hello', 'olleh'],
+      ['Is Fiber ready yet?', '?tey ydaer rebiF sI'],
+    ].forEach(([input, expected]) => {
+      expect(reverseString(input)).toBe(expected);
+    });
+  });
+});
