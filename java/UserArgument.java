b6bce4b5ff4b238304ee39754f632c130a2f670d
Thanik Bhongbhibhat <bhongbhibhat@gmail.com>
2017-09-22 15:13:57 -0700

    Intro to Programming in Java: 1.1 Your First Java Program

---
+++ java/UserArgument.java
@@ -0,0 +1,7 @@
+public class UserArgument {
+  public static void main(String[] args) {
+    System.out.print("Hi, ");
+    System.out.print(args[0]);
+    System.out.println(". How are you?");
+  }
+}
